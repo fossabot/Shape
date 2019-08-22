fbfacffc0c02f9ca51f341cd18160796a8835716
BenSummeryTime <51020968+BenSummeryTime@users.noreply.github.com>
2019-08-22 08:15:24 +0800

    Commit

---
+++ package/ShapeDOM/getElement.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright ©Summerytime 2019
+ * License Under MIT
+ */
+
+'use strict';
+/**
+ * getElements Model
+ */
+//INIT THE CHOOSE
+export function $_(SeleteElements) {
+    selete(SeleteElements);
+}
+var SeleteElements;
+//Begin GetElements;
+function selete(GetSeleteElements) {
+    if (GetSeleteElements == ) {
+
+    }
+    switch (GetSeleteElements) {
+        case:
+
+            break;
+
+        default: break;
+    }
+    return
+}
