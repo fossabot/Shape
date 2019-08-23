15331605e2568f6ef51cbf13d2bf1778180e55f8
BenSummeryTime <51020968+BenSummeryTime@users.noreply.github.com>
2019-08-23 08:14:45 +0800

    NewCommit

--- package/Shape.js
+++ package/Shape.js
@@ -1,8 +1,29 @@
 /**
  * Copyright ©Summerytime 2019
  * License Under MIT
+ * @param ReadHTMLElements as String
  */
 'use strict';
 (function(globel, factory) {
+    /**
+     * INIT
+     */
+    typeof exports == "object" && typeof module !== "undefined" ? module.exports = factory() :
+        typeof define === "function" && define.amd ? define(factory) : globel.shape = globel;
+}(this, function() {
+    /**
+     * HTMLElement Module
+     */
+    var HTMLElements = {};
 
-});
+    function isObject() {
+
+    }
+
+    function isHTMLElements(CheckBox) {
+
+    }
+    export function SeteleHTMLElements() {
+
+    }
+}));
